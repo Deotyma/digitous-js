faba97e80efb8a368315c491418473dddffd8a69
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-07 14:35:22 +0100

    exercice 2

---
+++ jour1.js
@@ -0,0 +1,20 @@
+/* exercice 1 */
+console.log("Hello World !");
+/* fin d'exercice 1 */
+
+/* exercice 2 */
+var test = "My name is Ewa"
+console.log(test)
+    /* fin d'exercice 2 */
+
+/* exercice 3 */
+/* fin d'exercice 3 */
+
+/* exercice 4 */
+/* fin d'exercice 4 */
+
+/* exercice  5*/
+/* fin d'exercice 5 */
+
+/* exercice 6 */
+/* fin d'exercice 6 */
