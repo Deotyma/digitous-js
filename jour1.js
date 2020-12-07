abf6a1e8d1a1d4d02b1d0e0a94fb9e98c2b47b91
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-07 14:40:55 +0100

    exercice 4

--- jour1.js
+++ jour1.js
@@ -3,16 +3,18 @@ console.log("Hello World !");
 /* fin d'exercice 1 */
 
 /* exercice 2 */
-var test = "My name is Ewa"
+var test = "My name is Ewa";
 console.log(test);
 /* fin d'exercice 2 */
 
 /* exercice 3 */
-var name = "Ewa"
+var name = "Ewa";
 console.log("Nice to meet you " + name);
 /* fin d'exercice 3 */
 
 /* exercice 4 */
+var testLength = "I'm very long !";
+console.log("Longeur d'un string: " + testLength.length);
 /* fin d'exercice 4 */
 
 /* exercice  5*/
