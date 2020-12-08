b14daeeda282173ed16920721b887d36f49a373b
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-08 14:05:05 +0100

    exercice 1

---
+++ jour2.js
@@ -0,0 +1,35 @@
+/* exercice 1 */
+console.log("ex1");
+var integer = 102;
+var float = 13.9;
+console.log(integer);
+console.log(float);
+/* fin d'exercice 1 */
+
+/* exercice 2 *4
+console.log("ex2");
+/* fin d'exercice 2 */
+
+/* exercice 3 */
+console.log("ex3");
+/* fin d'exercice 3 */
+
+/* exercice 4 */
+console.log("ex4");
+/* fin d'exercice 4 */
+
+/* exercice 5 */
+console.log("ex5");
+/* fin d'exercice 5 */
+
+/* exercice 7 */
+console.log("ex7");
+/* fin d'exercice 7 */
+
+/* exercice 7 */
+console.log("ex7");
+/* fin d'exercice 7 */
+
+/* exercice 8 */
+console.log("ex8");
+/* fin d'exercice 8 */
