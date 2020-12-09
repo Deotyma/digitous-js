e754b8ea4817cbeef2ccd7ce21dc8826d4d434ea
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-09 17:16:59 +0100

    bonus 1

--- jour3.js
+++ jour3.js
@@ -65,3 +65,21 @@ for (i = sentence.length - 1; i >= 0; i--) {
 }
 console.log(renversed);
 /* fin d'exercice 6 */
+
+/* bonus 1 */
+console.log("bonus 1");
+
+for (i = 0; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("fizz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+    } else if (i % 7 == 0) {
+        console.log(" ");
+    } else {
+        console.log(i);
+    }
+}
+/* fin bonus 1 */
