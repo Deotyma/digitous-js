72445aa1286aa54c7ed2513cd9705dfbd6c204db
Ewa Kadziolka <kalistaewa@gmail.com>
2020-12-17 12:25:05 +0100

    exercice 2

--- vacances.js
+++ vacances.js
@@ -2,8 +2,8 @@
 console.log("Exercice 1");
 
 function sortLetters(word) {
-    var letters = []
-    for (i = 0; i < word.length; i++) {
+    var letters = [];
+    for (var i = 0; i < word.length; i++) {
         letters.push(word[i]);
     }
     const regex = /,/gi;
@@ -15,9 +15,33 @@ sortLetters("konexio");
 
 /* fin d'exercice 1 */
 
-/* exercice 1 */
-console.log("Exercice 1");
-/* fin d'exercice 1 */
+/* exercice 2 */
+console.log("Exercice 2");
+
+function countEach(letters) {
+    letterX = [];
+    letterO = [];
+
+    for (var i = 0; i < letters.length; i++) {
+        if (letters[i] === "x" || letters[i] === "X") {
+            letterX.push(letters[i]);
+        } else if (letters[i] === "o" || letters[i] === "O") {
+            letterO.push(letters[i]);
+        }
+    }
+    var egality = letterO.length === letterX.length;
+    console.log(egality);
+
+    /* if (letterO.length === letterX.length) {
+        console.log(true);
+    } else {
+        console.log(false);
+    } */
+}
+
+countEach("oxoxx");
+
+/* fin d'exercice 2 */
 
 /* exercice 1 */
 console.log("Exercice 1");
