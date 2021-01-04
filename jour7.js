87ff80a7d353f4b3e5febc8e1756d1c4b4f6ff9f
Ewa Kadziolka <kalistaewa@gmail.com>
2021-01-04 14:21:01 +0100

    ex 1

---
+++ jour7.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+/* exercise 1 */
+console.log("exercise 1");
+fs.readFile("./jour7.txt", function(err, data) {
+    if (err) {
+        console.log("Il y a un erreur")
+    } else {
+        console.log(data.toString());
+
+    }
+});
+/* fin d'exercise 1 */
+
+/* exercise 2 */
+console.log("exercise 2");
+
+var array = [1, 2, 3, 4, 5]
+
+/* fin d'exercise 2 */
+4
+/* exercise 3 */
+console.log("exercise 3")
+    /* fin d'exercise 3 */
+
+/* exercise 4 */
+console.log("exercise 4")
+    /* fin d'exercise 4 */
+
+/* exercise 5 */
+console.log("exercise 5")
+    /* fin d'exercise 5 */
+
+/* exercice 6 */
+console.log("exercice 6")
+    /* fin d'exercice 6 */
