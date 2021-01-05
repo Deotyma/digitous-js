cafed2a40582795ccce7ebcfc26988f684bf7326
Ewa Kadziolka <kalistaewa@gmail.com>
2021-01-05 14:48:11 +0100

    exercice 2

--- jour8.js
+++ jour8.js
@@ -4,8 +4,8 @@ console.log("Exercice 1")
 var request = require('request');
 
 request.get("https://restcountries.eu/rest/v1/all", function(err, res, body) {
-    console.log(err);
-    console.log(res.statusCode);
+    /* console.log(err);
+    console.log(res.statusCode); */
     var infos = JSON.parse(body);
     /* console.log(infos); */
 
@@ -21,8 +21,23 @@ request.get("https://restcountries.eu/rest/v1/all", function(err, res, body) {
     getCountries();
     console.log(countriesNames.join(" - "));
 });
-
-
-
-
 /* fin d'exercice 1 */
+
+
+
+/* exercice 2 */
+console.log("exercice 2");
+
+request.get("https://api.chucknorris.io/jokes/random", function(err, res, body) {
+    /*  console.log(err);
+     console.log(res.statusCode); */
+    var jokes = JSON.parse(body);
+
+    /* console.log(jokes); */
+
+    var joke = jokes.value;
+    console.log(joke);
+});
+
+
+/* exercice 2 */
